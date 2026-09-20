cb82968def8df0692326ca36210346035b580275
Luis Soares <luissoaress.dev@gmail.com>
2026-09-20 10:55:32 -0300

    chore: initial commit and config environment variables

---
+++ src/test/java/com/DevShowcase/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DevShowcase.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
