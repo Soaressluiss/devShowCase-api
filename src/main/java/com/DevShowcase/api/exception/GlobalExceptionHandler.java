b519ba7b2c1b43c28b35006b76d32ba43a0e5f64
Luis Soares <luissoaress.dev@gmail.com>
2026-09-24 10:23:48 -0300

    feat: simplify validation error responses

--- src/main/java/com/DevShowcase/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/DevShowcase/api/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.DevShowcase.api.DTO.ErrorResponse;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -20,5 +21,22 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(
+            MethodArgumentNotValidException e
+    ) {
+        String message = e.getBindingResult()
+                .getFieldErrors()
+                .getFirst()
+                .getDefaultMessage();
+
+        ErrorResponse error = new ErrorResponse(
+                HttpStatus.BAD_REQUEST.value(),
+                message
+        );
+
+        return ResponseEntity.badRequest().body(error);
     }
 }
